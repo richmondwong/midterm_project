d1326e7a100df7b0c8fafdf342886ed2a09c8275
Mariam-RM <mariam.majeed.kes@gmail.com>
2018-12-20 17:40:19 +0000

    set up initial db query to return menu items to homepage and commented out unecessary routes and functions

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,10 +1,12 @@
-$(() => {
-  $.ajax({
-    method: "GET",
-    url: "/api/users"
-  }).done((users) => {
-    for(user of users) {
-      $("<div>").text(user.name).appendTo($("body"));
-    }
-  });;
-});
+// $(() => {
+//   $.ajax({
+//     method: "GET",
+//     url: "/api/users"
+//   }).done((users) => {
+//     for(user of users) {
+//       $("<div>").text(user.name).appendTo($("body"));
+//     }
+//   });;
+// });
+
+// remember AJAX should be used for single page stuff (think tweeter) and is called in the server.js file
--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const morgan      = require('morgan');
 const knexLogger  = require('knex-logger');
 
 // Seperated Routes for each Resource
-const usersRoutes = require("./routes/users");
+// const usersRoutes = require("./routes/users");
 
 // Load the logger first so all (static) HTTP requests are logged to STDOUT
 // 'dev' = Concise output colored by response status for development use.
@@ -36,13 +36,43 @@ app.use("/styles", sass({
 app.use(express.static("public"));
 
 // Mount all resource routes
-app.use("/api/users", usersRoutes(knex));
+// app.use("/api/users", usersRoutes(knex));
 
 // Home page
 app.get("/", (req, res) => {
-  res.render("index");
+
+    knex
+      .select("*")
+      .from("foods")
+      .then((results) => {
+        const menu = results;
+
+        console.log("this is the menu from knex query: ", menu);
+
+        const templateVars = {
+          menu: menu
+        };
+
+        return templateVars;
+
+      })
+      .then((template) =>{
+        res.render("index", template);
+      })
+      .catch((err)=>{
+        console.log(err)
+      })
+      // .finally(() => {
+      //        knex.destroy();
+      //     })
+
+  // res.render("index", templateVars);
 });
 
 app.listen(PORT, () => {
   console.log("Example app listening on port " + PORT);
 });
+
+
+
+
