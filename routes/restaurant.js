1dbb6482808d6a78ee40c5334ac5543f6aaebfd5
Richmond Wong <richmondwong@gmail.com>
2018-12-21 23:43:22 +0000

    Made changes to restaurant.js, restaurant_sum_layout.scss, and order_confirm and restaurant_summary EJS files

--- routes/restaurant.js
+++ routes/restaurant.js
@@ -15,32 +15,33 @@ module.exports = (knex) => {
   //RENDER order summary page for restaurant
   router.get("/summary", (req,res)=>{
 
-    let orderIdArr = [];
-    let templateVars = {};
 
 
     //SQL: Grab all order ids
     //SELECT orderid FROM orders;
     knex.select('orderid').from('orders')
       .then( (results) => {
+        let orderIdArr = [];
+
         results.forEach( (item) => {
           orderIdArr.push(item['orderid']);
         });
+
         console.log("orderid results:", orderIdArr);
+
         //SQL query from order# details:
         //SELECT * FROM "ordersFoods" JOIN foods ON "ordersFoods".foodid=foods.foodid WHERE orderid = 7
         knex.select('orderid', 'foods.foodid', 'foods.name', 'food_quantity')
           .from('ordersFoods')
           .join('foods', 'ordersFoods.foodid', '=', 'foods.foodid')
           .whereIn('orderid', orderIdArr)
-          .then((results) => {
-            templateVars = results;
-            //console.log("templateVars Data:", templateVars);
-            return templateVars;
-          })
-          .then( (templateVars) => {
-            console.log("temp:", templateVars);
-            res.render("restaurant_summary", {orders:templateVars});
+          .then( (orders) => {
+            // let groupedObjects = {};
+            // for(order of orders) {
+
+            // }
+            console.log("temp:", orders);
+            res.render("restaurant_summary", {orders});
           })
           .catch((err) => {
             console.log("Error @query for foods:", err);
