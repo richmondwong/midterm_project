b2aa99dc90cbdd180b4a0f414b33ad1349856bd2
Mariam-RM <mariam.majeed.kes@gmail.com>
2018-12-21 16:54:06 +0000

    rendering quantity as well

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -15,13 +15,35 @@ var basket = {}
 $(document).ready(function(){
 // jquery to send to cart if add to car button clicked
 
+
+function createItem(foodId){
+ let $food_item = $("<div class='food_item'></div>");
+
+ let name = `<div class="name">${basket[foodId].name}</div>`;
+ let price = `<div class="price">${basket[foodId].price}</div>`;
+ let quantity = `<div class="quantity">${basket[foodId].quantity}</div>`;
+
+
+ $food_item.append(name);
+ $food_item.append(price);
+ $food_item.append(quantity);
+
+ return $food_item
+}
+
 function renderBasket(basket){
+  $("#food_cart").empty();
+
+  for(foodId in basket){
+    var newFoodItem = createItem(foodId);
+    $("#food_cart").prepend(newFoodItem);
+  }
 
 }
 
-function deleteFromBasket(id){
+// function deleteFromBasket(id){
 
-}
+// }
 
   $(".add_to_cart").on("click", function(){
 
@@ -41,6 +63,8 @@ function deleteFromBasket(id){
     }
 
     console.log(basket);
+    renderBasket(basket)
+
 
 
   });
@@ -53,7 +77,7 @@ function deleteFromBasket(id){
 
 
     if( !basket[foodId]){
-      console.log("please add to cart first")
+      alert("please add to cart first")
     } else {
       if (basket[foodId].quantity === 1){
        delete basket[foodId];
@@ -64,6 +88,7 @@ function deleteFromBasket(id){
      }
     }
     console.log(basket);
+    renderBasket(basket)
 
 
   });
